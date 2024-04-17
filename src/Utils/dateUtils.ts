5997e1c7368cd9bd9fd57b53e74722acf3466056
Priyang <patelpriyang95@gmail.com>
2024-04-17 21:13:31 +0530

    vitest and tests for utils

--- src/Utils/dateUtils.ts
+++ src/Utils/dateUtils.ts
@@ -1,3 +1,13 @@
+/**
+ * Generates a unique key for a file based on its name and last modified date.
+ * @param {File} file - The file for which to generate the key.
+ * @returns {string} A unique key for the file.
+ * @example
+ * // Usage example:
+ * const file = new File(["content"], "example.txt", { type: "text/plain", lastModified: Date.now() });
+ * const key = getDBKey(file);
+ * console.log(key); // Output: "example.txt-2024-04-18T09:30:00.000Z"
+ */
 export const getDBKey = (file: File) => {
   const lastModifiedDate = new Date(file.lastModified);
   const dbKey = file.name + "-" + lastModifiedDate.toISOString();
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import type { InlineConfig } from "vitest";
+import type { UserConfig } from "vite";
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
   plugins: [react()],
-})
+} as VitestConfigExport);
