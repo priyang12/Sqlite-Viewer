cc76f6595d5599e2039b69bfed5574331b8f73a3
Priyang <patelpriyang95@gmail.com>
2024-04-18 01:12:29 +0530

    useDark test

---
+++ src/Hooks/__tests__/useDark.test.ts
@@ -0,0 +1,42 @@
+import { renderHook, act } from "@testing-library/react";
+import useDark from "../useDark";
+
+describe("useDark Hook", () => {
+  test("should be false by default (light mode)", () => {
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: vi.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addEventListener: vi.fn(),
+        removeEventListener: vi.fn(),
+        dispatchEvent: vi.fn(),
+      })),
+    });
+    const { result } = renderHook(() => useDark());
+
+    expect(result.current.isDarkMode).toBeFalsy();
+  });
+
+  test("should switch to dark mode when prefers-color-scheme is dark", () => {
+    // Mocking matchMedia to return dark mode preference
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: vi.fn().mockImplementation((query) => ({
+        matches: query === "(prefers-color-scheme: dark)",
+        media: query,
+        onchange: null,
+        addEventListener: vi.fn(),
+        removeEventListener: vi.fn(),
+        dispatchEvent: vi.fn(),
+      })),
+    });
+
+    // Re-render the hook with the mocked matchMedia
+    const { result } = renderHook(() => useDark());
+
+    // Assert that dark mode is enabled
+    expect(result.current.isDarkMode).toBeTruthy();
+  });
+});
