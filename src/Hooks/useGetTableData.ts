d0064a76dcabf4d8546c644027e5ea8afdc76f61
Priyang <patelpriyang95@gmail.com>
2024-04-26 22:36:21 +0530

    useGetTableData Hook

---
+++ src/Hooks/useGetTableData.ts
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { useDefaultGetDB } from "./useDefaultGetDB";
+import { SqlValue } from "sql.js";
+
+/**
+ * Custom hook to fetch data from a SQLite database using a specified query.
+ *
+ * @param {string} query - The SQL query to execute.
+ * @returns {Object} An object containing the fetched data rows, column names, and loading status.
+ * @example
+ * // Example usage:
+ * const { row, columns, loading } = useGetTableData("SELECT * FROM table");
+ * // 'row' contains the fetched data rows
+ * // 'columns' contains the column names of the fetched data
+ * // 'loading' indicates whether the data is currently being fetched
+ */
+export const useGetTableData = (query: string) => {
+  const { db } = useDefaultGetDB();
+  const [loading, setLoading] = useState(false);
+  const [columns, setColumns] = useState<string[]>();
+  const [row, setRow] = useState<SqlValue[][]>();
+
+  useEffect(() => {
+    if (db) {
+      try {
+        setLoading(true);
+        const result = db.exec(query);
+        const columns = result[0].columns;
+        const rows = result[0].values;
+        setColumns(columns);
+        setRow(rows);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
+      }
+    }
+    () => {
+      setColumns(undefined);
+      setRow(undefined);
+      setLoading(false);
+    };
+  }, [db, query]);
+
+  return {
+    row,
+    columns,
+    loading,
+  };
+};
