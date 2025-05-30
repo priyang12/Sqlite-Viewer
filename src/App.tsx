3a2e39f41137939dff5358348ba93be83459fd96
Priyang <patelpriyang95@gmail.com>
2025-05-30 22:21:48 +0530

    changed routing from component based to flat baed

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import {
-  BrowserRouter,
-  Navigate,
-  Outlet,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useEffect } from "react";
 import useDark from "./Hooks/useDark";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import UserDataBase from "./pages/UserDataBase/Tables";
-import Charts from "./pages/UserDataBase/Charts";
-import Builder from "./pages/UserDataBase/QueryBuilder";
-
-import { DBProvider } from "./Context/DBContext";
-
-function DBLayoutWrapper() {
-  return (
-    <DBProvider>
-      <Outlet />
-    </DBProvider>
-  );
-}
+import routes from "./routes";
 
 function App() {
   const { isDarkMode } = useDark();
@@ -32,23 +12,8 @@ function App() {
       ?.setAttribute("data-theme", isDarkMode ? "dark" : "light");
   }, [isDarkMode]);
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/db" element={<DBLayoutWrapper />}>
-          <Route path="tables/:name" element={<UserDataBase.DataBaseLayout />}>
-            <Route index element={<UserDataBase.Overview />} />
-            <Route path=":table" element={<UserDataBase.DatabaseTable />} />
-          </Route>
-          <Route path="charts/:name" element={<Charts.ChartDB />} />
-          <Route path="queryBuilder/:name" element={<Builder.QueryBuilder />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  const router = createBrowserRouter(routes);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/routes.tsx
@@ -0,0 +1,60 @@
+import { Navigate, Outlet } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import UserDataBase from "./pages/UserDataBase/Tables";
+import Charts from "./pages/UserDataBase/Charts";
+import Builder from "./pages/UserDataBase/QueryBuilder";
+import { DBProvider } from "./Context/DBContext";
+
+function DBLayoutWrapper() {
+  return (
+    <DBProvider>
+      <Outlet />
+    </DBProvider>
+  );
+}
+
+const routes = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/db",
+    element: <DBLayoutWrapper />,
+    children: [
+      {
+        path: "tables/:name",
+        element: <UserDataBase.DataBaseLayout />,
+        children: [
+          {
+            index: true,
+            element: <UserDataBase.Overview />,
+          },
+          {
+            path: ":table",
+            element: <UserDataBase.DatabaseTable />,
+          },
+        ],
+      },
+      {
+        path: "charts/:name",
+        element: <Charts.ChartDB />,
+      },
+      {
+        path: "queryBuilder/:name",
+        element: <Builder.QueryBuilder />,
+      },
+      {
+        path: "*",
+        element: <Navigate to="/" replace />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+];
+
+export default routes;
