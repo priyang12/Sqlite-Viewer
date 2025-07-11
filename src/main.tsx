2ed003da3d9d8becb1a4ad24a9484281543f393f
Priyang <patelpriyang95@gmail.com>
2025-07-11 18:57:26 +0530

    fixed tablenode styling issue

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import IndexedDBProvider from "./Context/IndexedDBContext.tsx";
-import "./index.css";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallbackComponent from "./Components/ErrorFallbackComponent";
+import "@xyflow/react/dist/style.css"; // this fixed the styling issue due to lazy loading
+// importing directly in main.ts rather than chart page.
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
